f1d080e85f9f0470ee5d5ae8171d2d0f51fb2c71
AsemBq <asem.b7460611@gmail.com>
2023-10-29 11:37:02 +0330

    try for js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.querySelector("li.AboutMe").addEventListener("click",()=>{
     document.querySelector(".sections section.active").classList.remove("active")
     document.querySelector("section.AboutMe").classList.add('active')
     
-    console.log("ok1")
 })
 
 document.querySelector("li.MySkilles").addEventListener("click",()=>{
@@ -12,7 +11,6 @@ document.querySelector("li.MySkilles").addEventListener("click",()=>{
     document.querySelector(".MySkilles").classList.add('active')
     document.querySelector(".sections section.active").classList.remove("active")
     document.querySelector('section.Tecnology').classList.add('active')
-    console.log("ok2")
 })
 
 document.querySelector("li.MyProject").addEventListener("click",()=>{
@@ -20,5 +18,28 @@ document.querySelector("li.MyProject").addEventListener("click",()=>{
     document.querySelector(".MyProject").classList.add('active')
     document.querySelector(".sections section.active").classList.remove("active")
     document.querySelector('section.MyProject').classList.add('active')
-    console.log("ok3")
+})
+
+
+
+// for mobile 
+document.querySelector("#About").addEventListener("click",()=>{
+    document.querySelector("nav.mobile-nav ul li.active").classList.remove("active");
+    document.querySelector("#About").classList.add('active');
+    document.querySelector(".sections section.active").classList.remove("active");
+    document.querySelector('section.Tecnology').classList.add('active');
+})
+
+document.querySelector("#Skilles").addEventListener("click",()=>{
+    document.querySelector("nav.mobile-nav ul li.active").classList.remove("active");
+    document.querySelector("#Skilles").classList.add('active');
+    document.querySelector(".sections section.active").classList.remove("active");
+    document.querySelector('section.Tecnology').classList.add('active');
+})
+
+document.querySelector("#projects").addEventListener("click",()=>{
+    document.querySelector("nav.mobile-nav ul li.active").classList.remove("active");
+    document.querySelector("#projects").classList.add('active');
+    document.querySelector(".sections section.active").classList.remove("active");
+    document.querySelector('section.Tecnology').classList.add('active');
 })
